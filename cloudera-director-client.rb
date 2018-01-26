abc3e81537c1ac59bfaeeb9a8863fac826ae1dc9
Takahiko Sato <takabow@gmail.com>
2018-01-26 19:39:26 +0900

    Cloudera Director 2.7.0

--- cloudera-director-client.rb
+++ cloudera-director-client.rb
@@ -1,8 +1,8 @@
 class ClouderaDirectorClient < Formula
   desc "Cloudera Director Client"
   homepage "https://www.cloudera.com/products/product-components/cloudera-director.html"
-  url "http://archive.cloudera.com/director/director/2/cloudera-director-client-2.6.1-director2.6.1.tar.gz"
-  sha256 "a32ca469fd2208ae75c0308f0398b28d0dd337b6789453e87af8e9fcdc776970"
+  url "http://archive.cloudera.com/director/director/2/cloudera-director-client-2.7.0-director2.7.0.tar.gz"
+  sha256 "60d40c0837a6ea0a27997805ae506200315caa9e01fbadb8809e60ac1a9bf989"
 
   def install
     libexec.install Dir["bin", "lib", "plugins", "etc"]
